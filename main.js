e69876a6e26885603d897a5f178f3310a7828c49
Mykola <nikomyk79@gmail.com>
2023-10-05 11:28:32 +0300

    added everything

---
+++ main.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs');
+fs.readFile("./data.json", "utf8", (err, jsonstring) => {
+    if (err) {
+        console.log("File read failed", err);
+        return;
+    }
+    const jsonData = JSON.parse(jsonstring);
+    const necessaryData = jsonData.map(item => {
+        if (item.ku === "13" && item.value > 5) {
+            return item.value;
+        }
+    });
+    const filteredData = necessaryData.filter(item => item !== undefined);
+    const finalString = JSON.stringify(filteredData);
+    fs.writeFile("output.txt", finalString, (err) => {
+        if (err) {
+            console.log("File write failed",err);
+            return;
+        }
+    })
+});
